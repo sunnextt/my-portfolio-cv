b6025b771b38ac8475a9e07a1f4513153a9554a0
sunnextt <sunnext2010@gmail.com>
2021-07-20 01:54:35 +0100

    add project

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,18 +1,39 @@
 import React from 'react';
+import Link from 'next/link'
+
 
 import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-
-  const handleClick = () => {
-  location.href = "www.linkedin.com/in/samyink";
-  }
-
+const style = {
+    boxSizing: "borderBox",
+    padding: 0,
+    lineHeight: 1.15,
+    margin: 0,
+    overflow: "visible",
+    textTransform: "none",
+    fontFamily: "Space Grotesk, sans-serif",
+    border: "none",
+    borderRadius: "50px",
+    color: "#fff",
+    display: "flex",
+    position: "absolute",
+    // top: 0,
+    left: 0,
+    width: "25rem",
+    height: "7rem",
+    background: "linear-gradient(270deg,#13ADC7 0%,#945DD6 100%)",
+    opacity: 1,
+    transition: "4s ease",
+    fontSize: "24px",
+    fontWeight: 600,
+    alignItems: "center",
+    justifyContent: "center",
+    cursor: "pointer",
+    boxShadow: "none",
+}
 
 const Hero = (props) => (
-
-  <>
     <Section row nopadding>
       <LeftSection>
         <SectionTitle main center>
@@ -20,12 +41,11 @@ const Hero = (props) => (
           My Personal Portfolio
         </SectionTitle>
         <SectionText>
-        A software engineer specialized in both front-end and back-end, experience creating custom websites through various software technologies such as JavaScript, typeScript, CSS, Redux, React.js, AWS and Node.js. 
+        A software engineer specialized in both front-end and back-end, experience creating custom websites through various software technologies such as JavaScript, typeScript, CSS, Redux, React.js, AWS and Node.js.
+        <a style={style} href="www.linkedin.com/in/samyink">Learn More</a>
         </SectionText>
-        <Button onClick={handleClick}>Learn More</Button>
       </LeftSection>
     </Section>
-  </>
 );
 
 export default Hero;
--- src/components/Hero/HeroStyles.js
+++ src/components/Hero/HeroStyles.js
@@ -17,3 +17,41 @@ export const LeftSection = styled.div`
     margin: 0 auto;
   }
 `;
+
+
+export const UtilityList = styled.ul`
+  list-style-type: none;
+  padding: 0;
+  display: flex;
+  justify-content: space-around;
+  margin: 2.5rem 0;
+`;
+
+export const ExternalLinks = styled.a`
+color:#d4c0c0;
+font-size: 1.6rem;
+padding:1rem 1.5rem;
+background: #6b3030;
+border-radius: 15px;
+transition: 0.5s;
+&:hover{
+  background: #801414;
+
+}
+`;
+
+// Navigation Links
+export const NavLink = styled.a`
+  font-size: 2rem;
+  line-height: 32px;
+  color: rgba(255, 255, 255, 0.75);
+  transition: 0.4s ease;
+  &:hover {
+    color: #fff;
+    opacity: 1;
+    cursor: pointer;
+  }
+  @media ${(props) => props.theme.breakpoints.sm} {
+    padding: 0.5rem;
+  }
+`;
